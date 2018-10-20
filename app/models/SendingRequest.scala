ca01ea11d0a13bd054d6c3793f85d84bd9fe2649
trimegisto <federico.lochbaum@gmail.com>
2018-10-20 11:12:42 -0300

    add json parse to SendingRequest

--- app/models/SendingRequest.scala
+++ app/models/SendingRequest.scala
@@ -1,4 +1,10 @@
 package models
 
+import play.api.libs.json.Json
+
 case class SendingRequest(id: Long, origin: String, destination: String, items: List[Item], isFragile: Boolean, refrigerationLevel: String)
 //owner: User, refrigerationLevel: RefrigerationLevel
+
+object SendingRequest {
+  implicit val sendinRequestFormat = Json.format[SendingRequest]
+}
